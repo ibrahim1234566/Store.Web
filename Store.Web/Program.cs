d5c5803b1fec3b66952594a941c026c3581b28d8
ibrahim1234566 <hemaazz123@gmail.com>
2024-10-11 18:36:19 +0300

    Repository Layer - UnitOfWork

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -1,6 +1,8 @@
 
 using Microsoft.EntityFrameworkCore;
 using Store.Data.Context;
+using Store.Repository.Interfaces;
+using Store.Repository.UnitOfWork;
 using Store.Web.Helper;
 
 namespace Store.Web
@@ -17,6 +19,7 @@ namespace Store.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped <IUnitOfWork,UnitOfWork>();
             builder.Services.AddDbContext<StoreDbContext>(options=>
             {
 
