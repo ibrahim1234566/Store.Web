287f834e4dc5962f53c6a7a80055ac33674cd93b
ibrahim1234566 <hemaazz123@gmail.com>
2024-10-15 04:06:36 +0300

    Apply Specifiecation & Base Specifications

---
+++ Store.Repository/Specification/ISpecification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Specification
+{
+    public interface ISpecification<T> 
+    {
+        Expression <Func<T,bool>> Criteria { get; }/// <summary>
+        /// for where conditions
+        /// </summary>
+        List<Expression<Func<T,object>>> Includes { get; }
+        ///
+        ///for includes
+        ///
+    }
+}
