d6b64d70a7aad0d5fbb075f2dedfed411b0f89d5
ibrahim1234566 <hemaazz123@gmail.com>
2024-10-25 23:35:38 +0300

    Seed Data To User

---
+++ Store.Repository/StoreIdentityContextSeed.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Store.Data.Entity.IdentityEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository
+{
+    public class StoreIdentityContextSeed
+    {
+        public static async Task SeedUserAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any() )
+            {
+                var user = new AppUser()
+                {
+                    DisplayName="Ibrahim EZZAT",
+                    Email="hemaazz123@gmail.com",
+                    UserName= "IbrahimEZZAT",
+                    address = new Address()
+                    {
+                        FirstName = "Ibrahim",
+                        LastName= "EZZAT",
+                        City ="Alex",
+                        State="Miami",
+                        Street="45",
+                        PostalCode="21644"
+
+                    }
+
+                };
+                await userManager.CreateAsync(user,"password123#");
+                
+            }
+        }
+    }
+}
